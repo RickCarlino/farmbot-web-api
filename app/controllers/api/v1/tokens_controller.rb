15de196a08fb96cdc395b1516f2a64c9032b64f1
rick carlino <rick.carlino@gmail.com>
2013-11-08 06:31:39 -0700

    Token creation and destruction specs are passing

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,5 +1,6 @@
 #Session controller provides a token
 class Api::V1::TokensController < ApplicationController
+  before_filter :set_user, only: [:destroy]
 
   def create
     @user = User.find_for_authentication(email: params[:email])
@@ -12,10 +13,20 @@ class Api::V1::TokensController < ApplicationController
   end
 
   def destroy
-    current_user.reset_authentication_token!
+    @api_user.reset_authentication_token!
+    head :no_content
   end
 
 private
+
+  def set_user
+    token = request.headers['FARMBOT-AUTH']
+    if 
+      @api_user = User.find_by(authentication_token: token)
+    else
+      head :unauthorized and return
+    end
+  end
 
   def user_params
     params.permit(:email, :password)
