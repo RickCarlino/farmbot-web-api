053cd8e88c900b55aeb737d4c137a3f8b1ff95eb
rick carlino <rick.carlino@gmail.com>
2013-11-08 06:36:31 -0700

    Token creation and destruction specs are passing

--- app/controllers/api/v1/tokens_controller.rb
+++ app/controllers/api/v1/tokens_controller.rb
@@ -1,6 +1,6 @@
 #Session controller provides a token
 class Api::V1::TokensController < ApplicationController
-  before_filter :set_user, only: [:destroy]
+  before_action :set_user, only: [:destroy]
 
   def create
     @user = User.find_for_authentication(email: params[:email])
@@ -18,15 +18,6 @@ class Api::V1::TokensController < ApplicationController
   end
 
 private
-
-  def set_user
-    token = request.headers['FARMBOT-AUTH']
-    if 
-      @api_user = User.find_by(authentication_token: token)
-    else
-      head :unauthorized and return
-    end
-  end
 
   def user_params
     params.permit(:email, :password)
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,17 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   # protect_from_forgery with: :null
-  skip_before_filter :verify_authenticity_token
+  skip_before_action :verify_authenticity_token
+
+
+  def set_user
+    token = request.headers['FARMBOT-AUTH']
+    if 
+      @api_user = User.find_by(authentication_token: token)
+    else
+      head :unauthorized and return
+    end
+  end
+
+
 end
