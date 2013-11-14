cd803105b7a11077a34227d877d327e98264f944
rick carlino <rick.carlino@gmail.com>
2013-11-13 20:35:44 -0700

    Adding more tests

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,32 @@ require 'spec_helper'
 describe User do
   let(:user){FactoryGirl.create(:user, email: 'test@test.com', password: 'password123')}
   #TODO: Write rspec tests for expected values of User attributes.
-  it 'has an email' do
-    user.email.should eq('test@test.com')
+  it ('has an email'){user.email.should eq('test@test.com')}
+
+  it('has a token'){user.ensure_authentication_token.should be}
+
+  it('has permissions'){user.permissions.should be_an(Array)}
+
+  it 'adds permissions' do
+    user.add_permission!('fake_controller#index')
+    user.permit?('fake_controller', 'index').should be_true
   end
 
-  it 'has a token' do
-    user.ensure_authentication_token.should be
+  it 'removes permissions' do
+    permission = user.permissions.first
+    user.remove_permission!(permission)
+    controller = permission.split('#').first
+    action     = permission.split('#').last
+    user.permit?(controller, action).should be_false
+  end
+
+  it 'returns false for non-permitted paths' do
+    user.permit?('fake', 'path').should be_false
+  end
+
+  it 'returns true for permitted paths' do
+    controller_name = User.default_permissions.first.split('#')[0] #EX: 'api/v1/users'
+    action_name     = User.default_permissions.first.split('#')[1] #EX: 'index', 'show', etc...
+    user.permit?(controller_name, action_name).should be_true
   end
 end
